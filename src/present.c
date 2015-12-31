3373121fc29b449345f51e586cb48c7f5beb93b9
Anewman2 <anewman@amenoph.us>
2015-12-30 21:30:18 -0500

    Added a mandala logo
    Courtesy of Aurora7054
    http://www.deviantart.com/art/Flower-Lotus-Mandala-46046992

--- src/present.c
+++ src/present.c
@@ -8,12 +8,28 @@
 #define WAKEUP_MIN_INTERVAL 1800
 #define WAKEUP_MAX_INTERVAL 5400
 
+// Define quiet times where app won't activate
+#define QUIET_TIME_START 22
+#define QUIET_TIME_END 7
+
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 static WakeupId s_wakeup_id;
+static GBitmap *s_lotus_bitmap;
+static BitmapLayer *s_bitmap_layer;
 
 static void schedule_wakeup() {
+	time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+	
+	// TEST: Run every 5 seconds
+	//time_t wakeup_time = time(NULL) + 5;
 	time_t wakeup_time = time(NULL) + (WAKEUP_MIN_INTERVAL + (rand() % WAKEUP_MAX_INTERVAL));
+	
+	if (tick_time->tm_hour >= QUIET_TIME_START || tick_time->tm_hour <= QUIET_TIME_END) {
+		// Jump ahead 10 hours. There's probably a smarter way to do this.
+		wakeup_time = time(NULL) + 6000;
+	}
 	
 	// Schedule event and store wakeup ID
 	s_wakeup_id = wakeup_schedule(wakeup_time, WAKEUP_REASON, true);
@@ -21,11 +37,23 @@ static void schedule_wakeup() {
 }
 
 static void wakeup_handler(WakeupId id, int32_t reason) {
-  // The app has woken!
-  text_layer_set_text(s_output_layer, "You are here.");
+	// The app has woken!
+	text_layer_set_text(s_output_layer, "You are here.");
+	vibes_short_pulse();
 
-  // Delete the ID
-  persist_delete(PERSIST_KEY_WAKEUP_ID);
+	// Create and display bitmap
+	Layer *window_layer = window_get_root_layer(s_main_window);
+	GRect bounds = layer_get_bounds(window_layer);
+
+	s_lotus_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LOTUS_MANDALA);
+
+	s_bitmap_layer = bitmap_layer_create(bounds);
+	bitmap_layer_set_bitmap(s_bitmap_layer, s_lotus_bitmap);
+	bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+	layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+
+	// Delete the ID
+	persist_delete(PERSIST_KEY_WAKEUP_ID);
 	
 	schedule_wakeup();
 }
@@ -39,7 +67,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
   text_layer_set_text(s_output_layer, "This application periodically sends notifications in the background. There's nothing to do in the main app.");
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
-	
+
 	// If wakeup is not already scheduled
 	if (!wakeup_query(s_wakeup_id, NULL)) {
 		schedule_wakeup();
@@ -49,6 +77,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   // Destroy output TextLayer
   text_layer_destroy(s_output_layer);
+	bitmap_layer_destroy(s_bitmap_layer);
+	gbitmap_destroy(s_lotus_bitmap);
 }
 
 static void init(void) {
